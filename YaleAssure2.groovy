52fb55325d533ee7e1f3525b300eb4dae97ac64d
sleuth255 <wilcox42787@gmail.com>
2026-08-12 07:08:41 -0500

    Update YaleAssure2.groovy
    add version info.

--- YaleAssure2.groovy
+++ YaleAssure2.groovy
@@ -1,6 +1,8 @@
 /**
  * 	Yale Assure Lock 2
  *
+ *  Version 1.00.00  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
